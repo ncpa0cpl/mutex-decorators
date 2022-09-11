fb2ac2b8e34002115fb0660c866b7565348bdf4e
ncpa0cpl <szymonb21@gmail.com>
2022-09-11 13:17:57 +0200

    fix: (build) deocrator type error

--- dist/types/repository-decorator.d.ts
+++ dist/types/repository-decorator.d.ts
@@ -1,1 +1,1 @@
-export declare const RWLockRepository: (RepositoryClass: new (...args: any) => any) => new (...args: any) => any;
+export declare const RWLockRepository: (RepositoryClass: new (...args: any) => any) => any;
